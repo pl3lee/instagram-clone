580c72905f1fad6e9748e763fab388738ad4fff3
pl3lee <billylee123@gmail.com>
2023-06-27 14:13:23 +0800

    caption

--- client/src/app/components/FollowingPostsContainer.tsx
+++ client/src/app/components/FollowingPostsContainer.tsx
@@ -45,7 +45,7 @@ const Post = async ({ post }: any) => {
 
 const PostHeader = ({ profilePicture, username }: any) => {
   return (
-    <div className="flex gap-2 px-2 py-3">
+    <div className="flex gap-1 px-2 py-3">
       <div className="flex-shrink-0 flex-grow-[1]">
         <img
           src={profilePicture}
@@ -109,10 +109,24 @@ const PostIconBar = () => {
 
 const PostInformation = ({ post, user }: any) => {
   return (
-    <div className="flex flex-col px-2 py-2">
-      <div>Likes: {post.likes.length}</div>
+    <div className="flex flex-col px-2 py-2 gap-1">
       <div>
-        {user.username} {post.caption}
+        <div className="flex items-center gap-1">
+          <span className="inline-flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+            </svg>
+          </span>
+          {post.likes.length} Likes
+        </div>
+      </div>
+      <div>
+        <span className="font-bold">{user.username}</span> {post.caption}
       </div>
     </div>
   );
