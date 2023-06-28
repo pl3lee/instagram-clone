b715d93a473c840506ce30b64d1113ee5dcaf556
pl3lee <billylee123@gmail.com>
2023-06-28 16:05:58 +0800

    removed unnecessary code

--- client/src/app/auth/signup/page.tsx
+++ client/src/app/auth/signup/page.tsx
@@ -1,17 +1,9 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { auth } from "../../../firebase/firebase-config";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import axios from "axios";
 
 const SignUp = () => {
   const router = useRouter();
