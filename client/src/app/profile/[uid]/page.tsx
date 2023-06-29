28615e6be9dfdf7dfefd7cfd18a117edc2a82a63
pl3lee <billylee123@gmail.com>
2023-06-29 21:44:22 +0800

    allows follows in profile

--- client/src/app/profile/[uid]/page.tsx
+++ client/src/app/profile/[uid]/page.tsx
@@ -1,20 +1,32 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import useLocalStorage from "use-local-storage";
 import useSWR from "swr";
 import axios from "axios";
+import { AuthContext } from "@/app/contexts/AuthContext";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const Profile = ({ params }: any) => {
-  const router = useRouter();
   const { uid } = params;
+
+  // if (!user) {
+  //   return <div></div>;
+  // }
+  // if (!isLoading) {
+  return <ProfileSection queriedUser={uid} />;
+  // }
+};
+
+const ProfileSection = ({ queriedUser }: any) => {
+  const router = useRouter();
+  const { refetchUser } = useContext(AuthContext);
   const [localuser, setLocaluser] = useLocalStorage("user", null);
   const [user, setUser] = useState(null);
   const { data, error, isLoading } = useSWR(
-    `http://localhost:3001/users/${uid}`,
+    `http://localhost:3001/users/${queriedUser}`,
     fetcher
   );
 
@@ -25,50 +37,80 @@ const Profile = ({ params }: any) => {
     setUser(localuser);
   }, [localuser]);
 
+  const handleFollow = () => {
+    axios
+      .patch("http://localhost:3001/users/follow", {
+        uid: user._id,
+        followId: queriedUser,
+      })
+      .then((res) => {
+        refetchUser();
+        console.log("User followed");
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleUnfollow = () => {
+    axios
+      .patch("http://localhost:3001/users/unfollow", {
+        uid: user._id,
+        followId: queriedUser,
+      })
+      .then((res) => {
+        refetchUser();
+        console.log("User followed");
+      })
+      .catch((err) => console.log(err));
+  };
   if (!user) {
     return <div></div>;
   }
   if (!isLoading) {
-    return <ProfileSection user={user} queriedUser={data} />;
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-center p-6 gap-8 w-full">
+          <div className="flex-shrink-0 flex justify-start">
+            <img
+              src={data.profilePicture}
+              className="w-[100px] h-[100px] rounded-full object-cover"
+            />
+          </div>
+          <div className="flex flex-shrink flex-col align-middle justify-center gap-3 p-1 w-[60%]">
+            <div className="text-3xl overflow-x-auto overflow-y-hidden no-scrollbar">
+              {data.username}
+            </div>
+            <div>
+              {data._id === user._id ? (
+                <button className="text-lg w-full text-center bg-slate-200 rounded-lg py-1 text-black">
+                  <Link href="/profile/edit">Edit Profile</Link>
+                </button>
+              ) : user.follows.includes(queriedUser) ? (
+                <button
+                  onClick={handleUnfollow}
+                  className="text-lg w-full text-center bg-slate-200 rounded-lg py-1 text-black"
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={handleFollow}
+                  className="text-lg w-full text-center bg-slate-200 rounded-lg py-1 text-black"
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6">{data.bio}</div>
+        <div className="w-full flex gap-3 justify-around border-y border-solid border-slate-300 py-4">
+          <BasicInfo num={data.posts.length} text="posts" />
+          <BasicInfo num={data.followers.length} text="followers" />
+          <BasicInfo num={data.follows.length} text="following" />
+        </div>
+      </div>
+    );
   }
-};
-
-const ProfileSection = ({ user, queriedUser }: any) => {
-  return (
-    <div className="flex flex-col gap-2">
-      <div className="flex justify-center p-6 gap-8 w-full">
-        <div className="flex-shrink-0 flex justify-start">
-          <img
-            src={queriedUser.profilePicture}
-            className="w-[100px] h-[100px] rounded-full object-cover"
-          />
-        </div>
-        <div className="flex flex-shrink flex-col align-middle justify-center gap-3 p-1 w-[60%]">
-          <div className="text-3xl overflow-x-auto overflow-y-hidden no-scrollbar">
-            {queriedUser.username}
-          </div>
-          <div>
-            {queriedUser._id === user._id ? (
-              <button className="text-lg w-full text-center bg-slate-200 rounded-lg py-1 text-black">
-                <Link href="/profile/edit">Edit Profile</Link>
-              </button>
-            ) : (
-              <button className="text-lg w-full text-center bg-slate-200 rounded-lg py-1 text-black">
-                Follow
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
-
-      <div className="p-6">{queriedUser.bio}</div>
-      <div className="w-full flex gap-3 justify-around border-y border-solid border-slate-300 py-4">
-        <BasicInfo num={queriedUser.posts.length} text="posts" />
-        <BasicInfo num={queriedUser.followers.length} text="followers" />
-        <BasicInfo num={queriedUser.follows.length} text="following" />
-      </div>
-    </div>
-  );
 };
 
 const BasicInfo = ({ num, text }: any) => {
