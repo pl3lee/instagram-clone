e18fb5512d3a333a287611ca13b57ee4aceec2d7
pl3lee <billylee123@gmail.com>
2023-06-30 16:26:53 +0800

    fixed double click

--- client/src/app/components/Post.tsx
+++ client/src/app/components/Post.tsx
@@ -14,6 +14,7 @@ const Post = ({ post }: any) => {
   const [localuser, setLocaluser] = useLocalStorage("user", null);
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [likeAmount, setLikeAmount] = useState(post.likes.length);
+  const [liked, setLiked] = useState(false);
   const {
     data: user,
     error,
@@ -23,6 +24,7 @@ const Post = ({ post }: any) => {
   useEffect(() => {
     if (localuser) {
       setLoggedInUser(localuser);
+      setLiked(post.likes.includes(localuser._id));
     }
     return () => {
       refetchUser();
@@ -39,6 +41,7 @@ const Post = ({ post }: any) => {
         } else if (res.data.message === "Post unliked") {
           setLikeAmount(likeAmount - 1);
         }
+        setLiked(!liked);
       })
       .catch((err) => {
         console.log(err);
@@ -54,10 +57,7 @@ const Post = ({ post }: any) => {
           uid={user?._id}
         />
         <PostImage img={post.img} toggleLike={handleToggleLike} />
-        <PostIconBar
-          toggleLike={handleToggleLike}
-          liked={post.likes.includes(localuser?._id)}
-        />
+        <PostIconBar toggleLike={handleToggleLike} liked={liked} />
         <PostInformation post={post} user={user} likeAmount={likeAmount} />
       </div>
     );
@@ -96,21 +96,19 @@ const PostImage = ({ img, toggleLike }: any) => {
 };
 
 const PostIconBar = ({ liked, toggleLike }: any) => {
-  const [updatedLiked, setUpdatedLiked] = useState(liked);
   return (
     <div className="flex">
       <ul className="px-2 py-2 flex gap-3 justify-start w-full bg-white dark:bg-black">
         <li className="icon-container p-0">
           <button
             onClick={(e) => {
-              setUpdatedLiked(!updatedLiked);
               toggleLike();
             }}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill={updatedLiked ? "red" : "none"}
+              fill={liked ? "red" : "none"}
               className="w-7 h-7 stroke-black dark:stroke-white"
             >
               <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
