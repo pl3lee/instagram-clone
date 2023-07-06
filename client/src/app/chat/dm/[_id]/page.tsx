a1778a3504bca4df4d4acc4e3bab2dc8b463a9de
pl3lee <billylee123@gmail.com>
2023-07-06 10:12:56 +0800

    added message input component, removed materialUI

--- client/src/app/chat/dm/[_id]/page.tsx
+++ client/src/app/chat/dm/[_id]/page.tsx
@@ -7,7 +7,8 @@ import { useEffect, useState } from "react";
 import { UserInterface } from "@/app/interfaces/User";
 import LoadingComponent from "@/app/components/LoadingComponent";
 import ProfilePictureIcon from "@/app/components/ProfilePictureIcon";
-import { Input } from "@rewind-ui/core";
+import { Input, InputRightElement, InputGroup } from "@chakra-ui/react";
+import MessageInput from "@/app/components/MessageInput";
 
 const DMChat = ({ params }: { params: { _id: string } }) => {
   const { _id } = params;
@@ -30,7 +31,10 @@ const DMChat = ({ params }: { params: { _id: string } }) => {
     }
   });
 
-  const handleSubmit = (e: React.FormEvent) => {};
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("submit");
+  };
 
   if (!localUserLoading && !chatroomUsersLoading && chatUser && localUser) {
     return (
@@ -39,34 +43,10 @@ const DMChat = ({ params }: { params: { _id: string } }) => {
         <div className="flex justify-center items-center bottom-0 left-0 fixed p-4 w-full">
           <div className="flex-grow-7 justify-start items-center flex">
             <form onSubmit={handleSubmit} className="w-full">
-              <Input
-                withRing={false}
-                color="gray"
-                className="rounded-full bg-black border border-borderGray"
-                value={chatInput}
-                onChange={(e) => setChatInput(e.target.value)}
+              <MessageInput
+                inputValue={chatInput}
+                setInputValue={setChatInput}
                 placeholder={`Message ${chatUser.username}`}
-                rightIcon={
-                  <button
-                    type="submit"
-                    className="justify-center flex items-center"
-                    disabled={chatInput.length === 0}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      className="w-6 h-6 stroke-black"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                      />
-                    </svg>
-                  </button>
-                }
               />
             </form>
           </div>
---
+++ client/src/app/components/MessageInput.tsx
@@ -0,0 +1,49 @@
+import { InputGroup, Input, InputRightElement } from "@chakra-ui/react";
+
+const MessageInput = ({
+  inputValue,
+  setInputValue,
+  placeholder,
+}: {
+  inputValue: string;
+  setInputValue: React.Dispatch<React.SetStateAction<string>>;
+  placeholder: string;
+}) => {
+  return (
+    <InputGroup>
+      <Input
+        variant="outline"
+        size="lg"
+        focusBorderColor="#ffffff0"
+        className="rounded-full bg-black border border-borderGray focus:outline-none"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder={placeholder}
+      />
+      <InputRightElement className="h-full">
+        <button
+          type="submit"
+          className="justify-center flex items-center h-full"
+          disabled={inputValue.length === 0}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            className={`w-6 h-6 stroke-white ${
+              inputValue.length === 0 ? "fill-none" : "fill-accentBlue"
+            }`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
+          </svg>
+        </button>
+      </InputRightElement>
+    </InputGroup>
+  );
+};
+
+export default MessageInput;
