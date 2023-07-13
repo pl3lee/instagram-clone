d71087ff7cf8578f881cb1f0c2bb627a6ed1b315
pl3lee <billylee123@gmail.com>
2023-07-13 14:09:53 +0800

    loading component fixed

--- client/src/app/components/LoadingComponent.tsx
+++ client/src/app/components/LoadingComponent.tsx
@@ -2,7 +2,7 @@
 import { CircularProgress } from "@chakra-ui/react";
 const LoadingComponent = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-full">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <CircularProgress isIndeterminate />
     </div>
   );
