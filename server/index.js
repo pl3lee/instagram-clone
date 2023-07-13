8103f5b751fa30be2ef51e550e92c629c84465e2
pl3lee <billylee123@gmail.com>
2023-07-13 10:27:05 +0800

    socketio origin

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ server.listen(process.env.PORT || 3001, () =>
 
 export const io = new SocketIOServer(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: "*",
   },
 });
 
