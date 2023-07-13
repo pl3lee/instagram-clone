7623246ea3cb60f778670a581efdbc15fa1aa8fc
pl3lee <billylee123@gmail.com>
2023-07-13 13:20:47 +0800

    env variables

--- server/firebase/firebase-config.js
+++ server/firebase/firebase-config.js
@@ -7,15 +7,15 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDuDe3vN6rQd7R62A1-f0gXZS9dxUyURhU",
-  authDomain: "instagram-clone-8545f.firebaseapp.com",
-  projectId: "instagram-clone-8545f",
-  storageBucket: "instagram-clone-8545f.appspot.com",
-  messagingSenderId: "729802950829",
-  appId: "1:729802950829:web:8a07a232993c805e26de24",
-  measurementId: "G-B9KQEDBJDE"
+  apiKey: process.env.firebaseApiKey,
+  authDomain: process.env.firebaseAuthDomain,
+  projectId: process.env.firebaseProjectId,
+  storageBucket: process.env.firebaseStorageBucket,
+  messagingSenderId: process.env.firebaseMessagingSenderId,
+  appId: process.env.firebaseAppId,
+  measurementId: process.env.firebaseMeasurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
